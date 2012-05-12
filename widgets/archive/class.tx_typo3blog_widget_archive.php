f6ed76f0d6e071010cc1e95149d9560cc846a1c1
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-12 09:44:57 +0000

    Made some changes to the setup #37109
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/typo3_blog/trunk@61915 735d13b6-9817-0410-8766-e36946ffe9aa

--- widgets/archive/class.tx_typo3blog_widget_archive.php
+++ widgets/archive/class.tx_typo3blog_widget_archive.php
@@ -241,8 +241,11 @@ class tx_typo3blog_widget_archive extends tslib_pibase
 		// Complete the template expansion by replacing the "content" marker in the template
 		$content .= $this->typo3BlogFunc->substituteMarkersAndSubparts($template, $markers, $subparts);
 
+		// wrap the content
+		$content = $this->cObj->stdWrap($content, $this->conf['stdWrap.']);
+
 		// Return the content to display in frontend
-		return $this->typo3BlogFunc->pi_wrapInBaseClass($content,$this->prefixId.'-widget_archive');
+		return $content;
 	}
 
 	/**
