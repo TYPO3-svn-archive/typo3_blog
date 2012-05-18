ed83262a9b5bb13bbac7a14f2bc6474e87c5b288
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-18 15:48:28 +0000

    First version of integration tt_content.search
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/typo3_blog/trunk@62439 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,6 +12,9 @@ t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_typo3blog_pi1.php', '_pi1', 'l
 $_EXTCONF = unserialize($_EXTCONF);
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['doktypeId'] = intval($_EXTCONF['doktypeId']) ? intval($_EXTCONF['doktypeId']) : 73;
 
+// Exclude the blogcategora pages from indexed_search
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['crawler']['excludeDoktype'][] = intval($_EXTCONF['doktypeId']) ? intval($_EXTCONF['doktypeId']) : 73;
+
 $GLOBALS["TYPO3_CONF_VARS"]["FE"]["addRootLineFields"] .= ',tx_typo3blog_blogrolls';
 
 $extensionPath = t3lib_extMgm::extPath('typo3_blog');
